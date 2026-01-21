ddd07b5db535bb134dc37fcde09f627d1da8fe94
EndsM <lonhongsy@gmail.com>
2026-01-21 18:32:43 +0800

    Start to work on Item structs

--- src-tauri/src/data/item.rs
+++ src-tauri/src/data/item.rs
@@ -1,1 +1,24 @@
-pub struct ItemData {}
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ItemData {
+    #[serde(rename = "type")]
+    pub data_type: String,
+    pub version: String,
+    pub basic: Item,
+    pub data: HashMap<String, Item>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Item {
+    #[serde(default)]
+    pub name: String,
+    #[serde(default)]
+    pub description: String,
+    #[serde(default)]
+    pub colloq: String,
+    #[serde(default)]
+    pub plaintext: String,
+}
