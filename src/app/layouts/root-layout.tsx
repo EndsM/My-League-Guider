697aa5b87d3370c0462418ef1118319ef4bd811e
EndsM <lonhongsy@gmail.com>
2026-09-23 16:59:50 +0800

    Wrap root-kayout with tooltip provider
    It is from new tooltip base

--- src/app/layouts/root-layout.tsx
+++ src/app/layouts/root-layout.tsx
@@ -5,21 +5,24 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
-    <SidebarProvider className="h-full w-full overflow-hidden">
-      <AppSidebar />
-      <SidebarInset className="flex h-full flex-col overflow-hidden">
-        <header className="flex h-10 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-        </header>
-        <main className="flex flex-1 flex-col overflow-hidden">
-          <Outlet />
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
+    <TooltipProvider>
+      <SidebarProvider className="h-full w-full overflow-hidden">
+        <AppSidebar />
+        <SidebarInset className="flex h-full flex-col overflow-hidden">
+          <header className="flex h-10 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </header>
+          <main className="flex flex-1 flex-col overflow-hidden">
+            <Outlet />
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </TooltipProvider>
   );
 }
